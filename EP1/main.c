bd3ed29588890b2a31ee650b96554f12701fe926
Caio Lopes <caio.lopes@tapps.com.br>
2018-05-23 21:56:16 -0300

    pthreads not working

--- EP1/main.c
+++ EP1/main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <math.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "functions.h"
 
@@ -10,10 +11,18 @@ typedef unsigned long long int64;
 
 typedef struct thread_args
 {
-	char species;
-	int id;
-	int pos;
-} thread_args;
+	double ** A;
+	double ** B;
+	double ** C;
+	int64 from_m;
+	int64 from_n;
+	int64 to_m;
+	int64 to_n;
+	int64 block_size;
+	int64 matrix_size;
+
+} thread_args, *thread_args_ptr;
+
 
 int min(int64 a, int64 b)
 {
@@ -81,12 +90,21 @@ double **multiplySequential(double **A, double **B, double **C,
 	return C;
 }
 
-// void multTest1(A, B, C, s,  ){
+void *multTest1(thread_args_ptr args){
 
-// 	for (int64 k = 0; k < s; k++)
-// 		multiplySequential(A, B, C, i * qr, j * qt, k * qs,
-// 						   min((i + 1) * qr, m), min((j + 1) * qt, n), min((k + 1) * qs, p));
-// }
+	double **A = args->A;
+	double **B = args->B;
+	double **C = args->C;
+	int64 from_m = args->from_m;
+	int64 from_n = args->from_n;
+	int64 to_m = args->to_m;
+	int64 to_n = args->to_n;
+	int64 block_size = args->block_size;
+	int64 matrix_size = args->matrix_size;
+
+	for (int64 k = 0; k < matrix_size; k+=block_size)
+		multiplySequential(A, B, C, from_m, from_n, k, to_m, to_n, min(k + block_size, matrix_size));
+}
 
 double **multiplyBlocks(double **A, double **B, double **C, int64 m, int64 n, int64 p){
 
@@ -101,12 +119,32 @@ double **multiplyBlocks(double **A, double **B, double **C, int64 m, int64 n, in
 	int t = 4;
 	int qt = n / t + (n % t != 0);
 
-	for (int64 i = 0; i < m; i+=qr)
-		for (int64 j = 0; j < n; j+=qt)
-			// pthread_create(&(THREADS[i]), NULL, multTest1, &(arguments[i]));
-			for (int64 k = 0; k < p; k+=qs)
-				multiplySequential(A, B, C, i, j, k,
-								   min(i + qr, m), min(j + qt, n), min(k + qs, p));
+	pthread_t threads[r*s];
+	thread_args threadsArgs[r*s];
+
+	for (int64 i = 0; i < m; i+=qr){
+		for (int64 j = 0; j < n; j+=qt){
+			int thread_index = 0;
+
+			threadsArgs[thread_index].A = A;
+			threadsArgs[thread_index].B = B;
+			threadsArgs[thread_index].C = C;
+			threadsArgs[thread_index].from_m = i;
+			threadsArgs[thread_index].from_n = j;
+			threadsArgs[thread_index].to_m = min(i + qr, m);
+			threadsArgs[thread_index].to_n = min(j + qt, n);
+			threadsArgs[thread_index].block_size = qs;
+			threadsArgs[thread_index].matrix_size = p;
+
+
+			pthread_create(&(threads[thread_index]), NULL, multTest1, &(threadsArgs[thread_index]));
+			// for (int64 k = 0; k < p; k+=qs)
+			// 	multiplySequential(A, B, C, i, j, k,
+			// 					   min(i + qr, m), min(j + qt, n), min(k + qs, p));
+
+			thread_index ++;
+		}
+	}
 
 	return C;
 }
@@ -155,11 +193,10 @@ int main(int argc, char* argv[]) {
 	double **A, **B, **C;
 	unsigned long long m, n, p;
 
-	A = readMatrix(&m, &p, "8x6");
-	B = readMatrix(&p, &n, "6x10");
+	A = readMatrix(&m, &p, "4x4A");
+	B = readMatrix(&p, &n, "4x4B");
 
 	// A = readMatrix(&m, &p, argv[1]);
-	// // printMatrix(A, m, p);
 	// B = readMatrix(&p, &n, argv[2]);
 
 	C = malloc(m * sizeof(double *));
@@ -170,7 +207,7 @@ int main(int argc, char* argv[]) {
 
 	multiplyBlocks(A, B, C, m, n, p);
 	// multiplySequential(A, B, C, 0, 0, 0, m, n, p);
-	saveMatrix(C, m, n, "blocks");
+	saveMatrix(C, m, n, "4x4C");
 
 	// double **M;
 	// M = generateRandomMatrix(6, 10);
